471f0e98d3cd3d1bb0410fe245f8b70ca38c7271
Michael Serhiienko <fleshrobot7@gmail.com>
2023-06-28 01:09:00 +0300

    cavans

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,152 +1,69 @@
-const canvas = document.getElementById("sunCanvas");
-const context = canvas.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
-const sunRadius = 100;
+var squareSize = 250;
+var squareX = (canvas.width - squareSize) / 2;
+var squareY = (canvas.height - squareSize) / 2;
 
-let rotationAngle = 0;
-let rayLength = 70;
-let increasing = true;
+var ballRadius = 20;
+var ballX = squareX + ballRadius;
+var ballY = squareY + ballRadius;
+var dx = Math.random() * 4 - 2;
+var dy = Math.random() * 4 - 2;
+var colors = ["red", "green", "blue", "yellow", "orange"];
 
-function drawSun() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  context.beginPath();
-  context.arc(centerX, centerY, sunRadius, 0, 2 * Math.PI);
-  context.fillStyle = "yellow";
-  context.fill();
-  context.closePath();
-
-  context.save();
-  context.translate(centerX, centerY);
-  context.rotate(rotationAngle);
-
-  // Первый ряд лучей
-  for (let i = 0; i < 12; i++) {
-    const angle = (2 * Math.PI * i) / 12;
-    const startX = sunRadius * Math.cos(angle);
-    const startY = sunRadius * Math.sin(angle);
-    const endX = (sunRadius + rayLength) * Math.cos(angle);
-    const endY = (sunRadius + rayLength) * Math.sin(angle);
-
-    const cp1X = (sunRadius + rayLength / 2) * Math.cos(angle + 0.1);
-    const cp1Y = (sunRadius + rayLength / 2) * Math.sin(angle + 0.1);
-    const cp2X = (sunRadius + rayLength / 2) * Math.cos(angle - 0.1);
-    const cp2Y = (sunRadius + rayLength / 2) * Math.sin(angle - 0.1);
-
-    const baseWidth = 10;
-
-    context.beginPath();
-    context.moveTo(startX - baseWidth / 2, startY);
-    context.bezierCurveTo(
-      cp1X - baseWidth / 2,
-      cp1Y - baseWidth / 4,
-      cp2X - baseWidth / 2,
-      cp2Y - baseWidth / 4,
-      endX - baseWidth / 2,
-      endY
-    );
-    context.lineTo(endX + baseWidth / 2, endY);
-    context.bezierCurveTo(
-      cp2X + baseWidth / 2,
-      cp2Y - baseWidth / 4,
-      cp1X + baseWidth / 2,
-      cp1Y - baseWidth / 4,
-      startX + baseWidth / 2,
-      startY
-    );
-    context.closePath();
-
-    const gradient = context.createRadialGradient(
-      0,
-      0,
-      sunRadius,
-      0,
-      0,
-      sunRadius + rayLength
-    );
-    gradient.addColorStop(0, "rgba(255, 255, 0, 1)");
-    gradient.addColorStop(1, "rgba(255, 255, 0, 0)");
-
-    context.fillStyle = gradient;
-    context.fill();
-    context.lineWidth = 2;
-    context.strokeStyle = "orange";
-    context.stroke();
-  }
-
-  // Второй ряд лучей
-  for (let i = 0; i < 24; i++) {
-    const angle = (2 * Math.PI * i) / 24;
-    const startX = (sunRadius - 10) * Math.cos(angle);
-    const startY = (sunRadius - 10) * Math.sin(angle);
-    const endX = (sunRadius + rayLength / 2) * Math.cos(angle);
-    const endY = (sunRadius + rayLength / 2) * Math.sin(angle);
-
-    const cp1X = (sunRadius + rayLength / 4) * Math.cos(angle + 0.05);
-    const cp1Y = (sunRadius + rayLength / 4) * Math.sin(angle + 0.05);
-    const cp2X = (sunRadius + rayLength / 4) * Math.cos(angle - 0.05);
-    const cp2Y = (sunRadius + rayLength / 4) * Math.sin(angle - 0.05);
-
-    const baseWidth = 5;
-
-    context.beginPath();
-    context.moveTo(startX - baseWidth / 2, startY);
-    context.bezierCurveTo(
-      cp1X - baseWidth / 2,
-      cp1Y - baseWidth / 4,
-      cp2X - baseWidth / 2,
-      cp2Y - baseWidth / 4,
-      endX - baseWidth / 2,
-      endY
-    );
-    context.lineTo(endX + baseWidth / 2, endY);
-    context.bezierCurveTo(
-      cp2X + baseWidth / 2,
-      cp2Y - baseWidth / 4,
-      cp1X + baseWidth / 2,
-      cp1Y - baseWidth / 4,
-      startX + baseWidth / 2,
-      startY
-    );
-    context.closePath();
-
-    const gradient = context.createRadialGradient(
-      0,
-      0,
-      sunRadius,
-      0,
-      0,
-      sunRadius + rayLength
-    );
-    gradient.addColorStop(0, "rgba(255, 255, 0, 1)");
-    gradient.addColorStop(1, "rgba(255, 255, 0, 0)");
-
-    context.fillStyle = gradient;
-    context.fill();
-    context.lineWidth = 2;
-    context.strokeStyle = "orange";
-    context.stroke();
-  }
-
-  context.restore();
-
-  rotationAngle += 0.01;
-
-  if (increasing) {
-    rayLength += 1;
-    if (rayLength > 90) {
-      increasing = false;
-    }
-  } else {
-    rayLength -= 1;
-    if (rayLength < 40) {
-      increasing = true;
-    }
-  }
-
-  requestAnimationFrame(drawSun);
+function drawSquare() {
+  ctx.beginPath();
+  ctx.rect(squareX, squareY, squareSize, squareSize);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.closePath();
 }
 
-drawSun();
+function drawBall(color) {
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+function updateBall() {
+  ballX += dx;
+  ballY += dy;
+
+  if (
+    ballX + ballRadius > squareX + squareSize ||
+    ballX - ballRadius < squareX
+  ) {
+    dx = -dx;
+    changeBallColor();
+  }
+  if (
+    ballY + ballRadius > squareY + squareSize ||
+    ballY - ballRadius < squareY
+  ) {
+    dy = -dy;
+    changeBallColor();
+  }
+}
+
+function changeBallColor() {
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  while (randomColor === "white") {
+    randomColor = colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  drawBall(randomColor);
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawSquare();
+  updateBall();
+  drawBall(colors[0]);
+  requestAnimationFrame(animate);
+}
+
+animate();
